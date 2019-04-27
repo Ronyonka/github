c39adcc3628523a06b31731c9c315bc2dffbaab7
Ronyonka <44805666+Ronyonka@users.noreply.github.com>
2019-04-27 21:24:00 +0300

    Update github-search.service.ts

--- src/app/github-request/github-search.service.ts
+++ src/app/github-request/github-search.service.ts
@@ -19,7 +19,7 @@ export class GithubSearchService {
   }
 
   getUserInfo(): any {
-    return this.http.get("http://api.github.com/users/" + this.username + "?access_token=488d0e41f7e4e997e6e6ad4db80f0bef6376e674")
+    return this.http.get("https://api.github.com/users/" + this.username + "?access_token=488d0e41f7e4e997e6e6ad4db80f0bef6376e674")
       .pipe(map(res => res));
   }
   getUserRepos(): any {
